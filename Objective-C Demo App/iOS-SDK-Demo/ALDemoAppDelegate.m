6c9c1630557738b051ed5041b46fe0c260c6cb37
Monica Ong <myo3@cornell.edu>
2017-06-02 13:30:17 -0700

    Test mode set on Swift & Objective-C app

--- Objective-C Demo App/iOS-SDK-Demo/ALDemoAppDelegate.m
+++ Objective-C Demo App/iOS-SDK-Demo/ALDemoAppDelegate.m
@@ -21,6 +21,8 @@
     // Initializing our SDK at launch is very important as it'll start preloading ads in the background.
     [ALSdk initializeSdk];
     
+    [ALSdk shared].settings.isTestAdsEnabled = YES;
+    
     [[UINavigationBar appearance] setTranslucent: NO];
     [[UINavigationBar appearance] setBarTintColor: [UIColor colorWithRed: 10.0f/255.0f green: 131.0f/255.f blue: 170.0f/255.0f alpha: 1.0f]];
     [[UINavigationBar appearance] setTitleTextAttributes: @{NSForegroundColorAttributeName : [UIColor whiteColor]}];
