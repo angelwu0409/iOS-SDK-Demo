f1d71ac4f971bccb0cbf09414d246e38d9bd635f
Monica Ong <myo3@cornell.edu>
2017-06-05 17:09:10 -0700

    Completed event tracking for Objective-C app

--- Objective-C Demo App/iOS-SDK-Demo/ALEventTrackingViewController.m
+++ Objective-C Demo App/iOS-SDK-Demo/ALEventTrackingViewController.m
@@ -10,7 +10,7 @@
 #import <StoreKit/StoreKit.h>
 
 #if __has_include(<AppLovinSDK/AppLovinSDK.h>)
-    #import <AppLovinSDK/AppLovinSDK.h>
+#import <AppLovinSDK/AppLovinSDK.h>
 #endif
 
 @interface ALEventTrackingViewController ()
@@ -32,7 +32,6 @@ static SKProduct *product = nil;
     }
     if (!transaction){
         transaction = [[SKPaymentTransaction alloc] init];
-    
     }
     if (!product){
         product = [[SKProduct alloc] init];
@@ -46,6 +45,134 @@ static SKProduct *product = nil;
                                         kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID",
                                         kALEventParameterRevenueAmountKey     : @"PRICE OF ITEM",
                                         kALEventParameterRevenueCurrencyKey   : @"3-LETTER CURRENCY CODE"
+                                        }
+             ];
+            break;
+        case 1:
+            [self setTitle: @"User added item to cart"];
+            [eventService trackEvent: kALEventTypeUserAddedItemToCart
+                          parameters: @{
+                                        kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID"
+                                        }
+             ];
+            break;
+        case 2:
+            [self setTitle: @"User completed achievement"];
+            [eventService trackEvent: kALEventTypeUserCompletedAchievement
+                          parameters: @{
+                                        kALEventParameterCompletedAchievementKey : @"ACHIEVEMENT NAME OR ID"
+                                        }
+             ];
+            break;
+        case 3:
+            [self setTitle: @"User completed checkout"];
+            [eventService trackEvent: kALEventTypeUserCompletedCheckOut
+                          parameters: @{
+                                        kALEventParameterCheckoutTransactionIdentifierKey : @"UNIQUE TRANSACTION ID",
+                                        kALEventParameterProductIdentifierKey     : @"PRODUCT SKU OR ID",
+                                        kALEventParameterRevenueAmountKey         : @"AMOUNT OF MONEY SPENT",
+                                        kALEventParameterRevenueCurrencyKey       : @"3-LETTER CURRENCY CODE"
+                                        }
+             ];
+            break;
+        case 4:
+            [self setTitle: @"User completed level"];
+            [eventService trackEvent: kALEventTypeUserCompletedLevel
+                          parameters: @{
+                                        kALEventParameterCompletedLevelKey : @"LEVEL NAME OR NUMBER"
+                                        }
+             ];
+            break;
+        case 5:
+            [self setTitle: @"User created reservation"];
+            [eventService trackEvent: kALEventTypeUserCreatedReservation
+                          parameters: @{
+                                        kALEventParameterProductIdentifierKey     : @"PRODUCT SKU OR ID",
+                                        kALEventParameterReservationStartDateKey  : @"START NSDATE",
+                                        kALEventParameterReservationEndDateKey    : @"END NSDATE"
+                                        }
+             ];
+            break;
+        case 6:
+            [self setTitle: @"Tracking in app purchase"];
+            [eventService trackInAppPurchaseWithTransactionIdentifier: transaction.transactionIdentifier
+                                                           parameters: @{
+                                                                         kALEventParameterRevenueAmountKey : @"AMOUNT OF MONEY SPENT",
+                                                                         kALEventParameterRevenueCurrencyKey : @"3-LETTER CURRENCY CODE",
+                                                                         kALEventParameterProductIdentifierKey : @"product.productIdentifier"
+                                                                         }
+             ];
+            break;
+        case 7:
+            [self setTitle: @"User logged in"];
+            [eventService trackEvent: kALEventTypeUserLoggedIn
+                          parameters: @{
+                                        kALEventParameterUserAccountIdentifierKey : @"USERNAME"
+                                        }
+             ];
+            break;
+        case 8:
+            [self setTitle: @"User provided payment information"];
+            [eventService trackEvent: kALEventTypeUserProvidedPaymentInformation];
+            break;
+        case 9:
+            [self setTitle: @"User created account"];
+            [eventService trackEvent: kALEventTypeUserCreatedAccount
+                          parameters: @{
+                                        kALEventParameterUserAccountIdentifierKey : @"USERNAME"
+                                        }
+             ];
+            break;
+        case 10:
+            [self setTitle: @"User executed search"];
+            [eventService trackEvent: kALEventTypeUserExecutedSearch
+                          parameters: @{
+                                        kALEventParameterSearchQueryKey : @"USER'S SEARCH STRING"
+                                        }
+             ];
+            break;
+        case 11:
+            [self setTitle: @"User sent invitation"];
+            [eventService trackEvent: kALEventTypeUserSentInvitation];
+            break;
+        case 12:
+            [self setTitle: @"User shared link"];
+            [eventService trackEvent: kALEventTypeUserSharedLink];
+            break;
+        case 13:
+            [self setTitle: @"User spent virtual currency"];
+            [eventService trackEvent: kALEventTypeUserSpentVirtualCurrency
+                          parameters: @{
+                                        kALEventParameterVirtualCurrencyAmountKey : @"NUMBER OF COINS SPENT",
+                                        kALEventParameterVirtualCurrencyNameKey : @"CURRENCY NAME"
+                                        }
+             ];
+            break;
+        case 14:
+            [self setTitle: @"User completed tutorial"];
+            [eventService trackEvent: kALEventTypeUserCompletedTutorial];
+            break;
+        case 15:
+            [self setTitle: @"User viewed content"];
+            [eventService trackEvent: kALEventTypeUserViewedContent
+                          parameters: @{
+                                        kALEventParameterContentIdentifierKey : @"SOME ID DESCRIBING CONTENT"
+                                        }
+             ];
+            break;
+        case 16:
+            [self setTitle: @"User viewed product"];
+            [eventService trackEvent: kALEventTypeUserViewedProduct
+                          parameters: @{
+                                        kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID"
+                                        }
+             ];
+            break;
+        case 17:
+            [self setTitle: @"User added item to wishlist"];
+            [eventService trackEvent: kALEventTypeUserAddedItemToWishlist
+                          parameters: @{
+                                        kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID"
                                         }
              ];
             break;
