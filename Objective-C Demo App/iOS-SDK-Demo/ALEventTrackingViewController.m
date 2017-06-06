5e760233741894c92903d25aa203957ecb0488d0
Monica Ong <myo3@cornell.edu>
2017-06-06 14:48:33 -0700

    Dynamically add event tracking cells

--- Objective-C Demo App/iOS-SDK-Demo/ALEventTrackingViewController.m
+++ Objective-C Demo App/iOS-SDK-Demo/ALEventTrackingViewController.m
@@ -10,35 +10,87 @@
 #import <StoreKit/StoreKit.h>
 
 #if __has_include(<AppLovinSDK/AppLovinSDK.h>)
-#import <AppLovinSDK/AppLovinSDK.h>
+    #import <AppLovinSDK/AppLovinSDK.h>
+#else
+    #import "ALSdk.h"
+    #import "ALEventService.h"
 #endif
 
-@interface ALEventTrackingViewController ()
+@interface ALDemoEvent : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *purpose;
+
+- (instancetype)initWithName:(NSString *)name purpose:(NSString *)purpose;
 
 @end
 
+@interface ALEventTrackingViewController()
+@property (nonatomic, strong) NSArray<ALDemoEvent *> *events;
+@end
+
 @implementation ALEventTrackingViewController
-static ALEventService *eventService = nil;
-//In-app Purchases
-static SKPaymentTransaction *transaction = nil;
-static SKProduct *product = nil;
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+        
+    self.events = @[
+                    [[ALDemoEvent alloc] initWithName: @"Began Checkout Event" purpose: @"Track when user begins checkout procedure"],
+                    [[ALDemoEvent alloc] initWithName: @"Cart Event" purpose: @"Track when user adds an item to cart"],
+                    [[ALDemoEvent alloc] initWithName: @"Completed Achievement Event" purpose: @"Track when user completed an achievement"],
+                    [[ALDemoEvent alloc] initWithName: @"Completed Checkout Event" purpose: @"Track when user completed checkout"],
+                    [[ALDemoEvent alloc] initWithName: @"Completed Level Event" purpose: @"Track when user completed level"],
+                    [[ALDemoEvent alloc] initWithName: @"Created Reservation Event" purpose: @"Track when user created a reservation"],
+                    [[ALDemoEvent alloc] initWithName: @"In-App Purchase Event" purpose: @"Track when user makes an in-app purchase"],
+                    [[ALDemoEvent alloc] initWithName: @"Login Event" purpose: @"Track when user logs in"],
+                    [[ALDemoEvent alloc] initWithName: @"Payment Info Event" purpose: @"Tracks when user inputs their payment information"],
+                    [[ALDemoEvent alloc] initWithName: @"Registration Event" purpose: @"Track when user registers"],
+                    [[ALDemoEvent alloc] initWithName: @"Search Event" purpose: @"Track when user makes a search"],
+                    [[ALDemoEvent alloc] initWithName: @"Sent Invitation Event" purpose: @"Track when user sends invitation"],
+                    [[ALDemoEvent alloc] initWithName: @"Shared Link Event" purpose: @"Track when user shares a link"],
+                    [[ALDemoEvent alloc] initWithName: @"Spent Virtual Currency Event" purpose: @"Track when users spends virtual currency"],
+                    [[ALDemoEvent alloc] initWithName: @"Tutorial Event" purpose: @"Track when users does a tutorial"],
+                    [[ALDemoEvent alloc] initWithName: @"Viewed Content Event" purpose: @"Track when user views content"],
+                    [[ALDemoEvent alloc] initWithName: @"Viewed Product Event" purpose: @"Track when user views product"],
+                    [[ALDemoEvent alloc] initWithName: @"Wishlist Event" purpose: @"Track when user adds an item to their wishlist"]
+                    ];
+}
+
+#pragma mark - Table View Data Source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.events.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: @"rootPrototype"];
+    if(!cell){
+        cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleSubtitle reuseIdentifier: @"rootPrototype"];
+    }
+    
+    cell.textLabel.text = self.events[indexPath.row].name;
+    cell.detailTextLabel.text = self.events[indexPath.row].purpose;
+    
+    return cell;
+    
+}
+
+#pragma mark - Table View Delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [super tableView: tableView didSelectRowAtIndexPath: indexPath];
     
-    if (!eventService){
-        eventService = [ALSdk shared].eventService;
-    }
-    if (!transaction){
-        transaction = [[SKPaymentTransaction alloc] init];
-    }
-    if (!product){
-        product = [[SKProduct alloc] init];
-    }
+    ALEventService *eventService = [ALSdk shared].eventService;
     
-    switch (indexPath.row) {
-        case 0:
+    switch (indexPath.row)
+    {
+        case 0: {
             [self setTitle: @"User began checkout"];
             [eventService trackEvent: kALEventTypeUserBeganCheckOut
                           parameters: @{
@@ -48,7 +100,8 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 1:
+        }
+        case 1: {
             [self setTitle: @"User added item to cart"];
             [eventService trackEvent: kALEventTypeUserAddedItemToCart
                           parameters: @{
@@ -56,7 +109,8 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 2:
+        }
+        case 2: {
             [self setTitle: @"User completed achievement"];
             [eventService trackEvent: kALEventTypeUserCompletedAchievement
                           parameters: @{
@@ -64,7 +118,8 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 3:
+        }
+        case 3: {
             [self setTitle: @"User completed checkout"];
             [eventService trackEvent: kALEventTypeUserCompletedCheckOut
                           parameters: @{
@@ -75,7 +130,8 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 4:
+        }
+        case 4: {
             [self setTitle: @"User completed level"];
             [eventService trackEvent: kALEventTypeUserCompletedLevel
                           parameters: @{
@@ -83,7 +139,8 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 5:
+        }
+        case 5: {
             [self setTitle: @"User created reservation"];
             [eventService trackEvent: kALEventTypeUserCreatedReservation
                           parameters: @{
@@ -93,17 +150,21 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 6:
+        }
+        case 6: {
             [self setTitle: @"Tracking in app purchase"];
+            SKPaymentTransaction *transaction = [[SKPaymentTransaction alloc] init]; // from paymentQueue:updatedTransactions:
+            //SKProduct* product = ...; // Appropriate product (matching productIdentifier property to SKPaymentTransaction);
             [eventService trackInAppPurchaseWithTransactionIdentifier: transaction.transactionIdentifier
                                                            parameters: @{
                                                                          kALEventParameterRevenueAmountKey : @"AMOUNT OF MONEY SPENT",
                                                                          kALEventParameterRevenueCurrencyKey : @"3-LETTER CURRENCY CODE",
-                                                                         kALEventParameterProductIdentifierKey : @"product.productIdentifier"
+                                                                         kALEventParameterProductIdentifierKey : @"product.productIdentifier" //product.productIdentifier
                                                                          }
              ];
             break;
-        case 7:
+        }
+        case 7: {
             [self setTitle: @"User logged in"];
             [eventService trackEvent: kALEventTypeUserLoggedIn
                           parameters: @{
@@ -111,11 +172,13 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 8:
+        }
+        case 8: {
             [self setTitle: @"User provided payment information"];
             [eventService trackEvent: kALEventTypeUserProvidedPaymentInformation];
             break;
-        case 9:
+        }
+        case 9: {
             [self setTitle: @"User created account"];
             [eventService trackEvent: kALEventTypeUserCreatedAccount
                           parameters: @{
@@ -123,7 +186,8 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 10:
+        }
+        case 10: {
             [self setTitle: @"User executed search"];
             [eventService trackEvent: kALEventTypeUserExecutedSearch
                           parameters: @{
@@ -131,15 +195,18 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 11:
+        }
+        case 11: {
             [self setTitle: @"User sent invitation"];
             [eventService trackEvent: kALEventTypeUserSentInvitation];
             break;
-        case 12:
+        }
+        case 12: {
             [self setTitle: @"User shared link"];
             [eventService trackEvent: kALEventTypeUserSharedLink];
             break;
-        case 13:
+        }
+        case 13: {
             [self setTitle: @"User spent virtual currency"];
             [eventService trackEvent: kALEventTypeUserSpentVirtualCurrency
                           parameters: @{
@@ -148,11 +215,13 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 14:
+        }
+        case 14: {
             [self setTitle: @"User completed tutorial"];
             [eventService trackEvent: kALEventTypeUserCompletedTutorial];
             break;
-        case 15:
+        }
+        case 15: {
             [self setTitle: @"User viewed content"];
             [eventService trackEvent: kALEventTypeUserViewedContent
                           parameters: @{
@@ -160,7 +229,8 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 16:
+        }
+        case 16: {
             [self setTitle: @"User viewed product"];
             [eventService trackEvent: kALEventTypeUserViewedProduct
                           parameters: @{
@@ -168,7 +238,8 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        case 17:
+        }
+        case 17: {
             [self setTitle: @"User added item to wishlist"];
             [eventService trackEvent: kALEventTypeUserAddedItemToWishlist
                           parameters: @{
@@ -176,10 +247,27 @@ static SKProduct *product = nil;
                                         }
              ];
             break;
-        default:
+        }
+        default: {
             [self setTitle: @"Default event tracking initiated"];
             break;
+        }
     }
+}
+
+@end
+
+@implementation ALDemoEvent
+
+- (instancetype)initWithName:(NSString *)name purpose:(NSString *)purpose
+{
+    self = [super init];
+    if ( self )
+    {
+        self.name = name;
+        self.purpose = purpose;
+    }
+    return self;
 }
 
 @end
--- Swift Demo App/iOS-SDK-Demo/ALEventTrackingViewController.swift
+++ Swift Demo App/iOS-SDK-Demo/ALEventTrackingViewController.swift
@@ -10,24 +10,77 @@ import UIKit
 import StoreKit
 import AppLovinSDK
 
+struct ALDemoEvent
+{
+    var name: String
+    var purpose: String
+}
+
 class ALEventTrackingViewController: ALDemoBaseTableViewController
 {
-    let eventService: ALEventService = (ALSdk.shared()?.eventService)!
+    var events: [ALDemoEvent] = []
     
-    //In-App Purchases
-    let transaction: SKPaymentTransaction = SKPaymentTransaction() // from paymentQueue:updatedTransactions:
-    let product: SKProduct = SKProduct() // Appropriate product (matching productIdentifier property to SKPaymentTransaction)
+    // MARK: View Lifecycle
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        events = [ALDemoEvent(name: "Began Checkout Event", purpose: "Track when user begins checkout procedure"),
+                  ALDemoEvent(name: "Cart Event", purpose: "Track when user adds an item to cart"),
+                  ALDemoEvent(name: "Completed Achievement Event", purpose: "Track when user completed an achievement"),
+                  ALDemoEvent(name: "Completed Checkout Event", purpose: "Track when user completed checkout"),
+                  ALDemoEvent(name: "Completed Level Event", purpose: "Track when user completed level"),
+                  ALDemoEvent(name: "Created Reservation Event", purpose: "Track when user created a reservation"),
+                  ALDemoEvent(name: "In-App Purchase Event", purpose: "Track when user makes an in-app purchase"),
+                  ALDemoEvent(name: "Login Event", purpose: "Track when user logs in"),
+                  ALDemoEvent(name: "Payment Info Event", purpose: "Tracks when user inputs their payment information"),
+                  ALDemoEvent(name: "Registration Event", purpose: "Track when user registers"),
+                  ALDemoEvent(name: "Search Event", purpose: "Track when user makes a search"),
+                  ALDemoEvent(name: "Sent Invitation Event", purpose: "Track when user sends invitation"),
+                  ALDemoEvent(name: "Shared Link Event", purpose: "Track when user shares a link"),
+                  ALDemoEvent(name: "Spent Virtual Currency Event", purpose: "Track when users spends virtual currency"),
+                  ALDemoEvent(name: "Tutorial Event", purpose: "Track when users does a tutorial"),
+                  ALDemoEvent(name: "Viewed Content Event", purpose: "Track when user views content"),
+                  ALDemoEvent(name: "Viewed Product Event", purpose: "Track when user views product"),
+                  ALDemoEvent(name: "Wishlist Event", purpose: "Track when user adds an item to their wishlist")
+                 ]
+    }
+    
+    // MARK: Table View Data Source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return events.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "rootPrototype")
+        if(cell == nil){
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "rootPrototype")
+        }
+        
+        cell?.textLabel?.text = events[indexPath.row].name
+        cell?.detailTextLabel?.text = events[indexPath.row].purpose
+        
+        return cell!
+    }
+    
+    // MARK: Table View Delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
         super.tableView(tableView, didSelectRowAt: indexPath)
         
+        let eventService: ALEventService = (ALSdk.shared()?.eventService)!
+
         switch indexPath.row {
         case 0:
             title = "User began checkout"
             eventService.trackEvent(kALEventTypeUserBeganCheckOut,
                                     parameters: [kALEventParameterProductIdentifierKey : "PRODUCT SKU OR ID",
                                                  kALEventParameterRevenueAmountKey     : "PRICE OF ITEM",
-                                                 kALEventParameterRevenueCurrencyKey   :  "3-LETTER CURRENCY CODE"])
+                                                 kALEventParameterRevenueCurrencyKey   : "3-LETTER CURRENCY CODE"])
         case 1:
             title = "User added item to cart"
             eventService.trackEvent(kALEventTypeUserAddedItemToCart,
@@ -57,10 +110,13 @@ class ALEventTrackingViewController: ALDemoBaseTableViewController
                                                  kALEventParameterReservationEndDateKey   : "END NSDATE"])
         case 6:
             title = "Tracking in app purchase"
+            //In-App Purchases
+            // let transaction: SKPaymentTransaction = ... // from paymentQueue:updatedTransactions:
+            //let product: SKProduct = ... // Appropriate product (matching productIdentifier property to SKPaymentTransaction)
             eventService.trackInAppPurchase(withTransactionIdentifier: "transaction.transactionIdentifier",
                                             parameters: [kALEventParameterRevenueAmountKey     : "AMOUNT OF MONEY SPENT",
                                                          kALEventParameterRevenueCurrencyKey   : "3-LETTER CURRENCY CODE",
-                                                         kALEventParameterProductIdentifierKey : "product.productIdentifier"])
+                                                         kALEventParameterProductIdentifierKey : "product.productIdentifier"]) //product.productIdentifier
         case 7:
             title = "User logged in"
             eventService.trackEvent(kALEventTypeUserLoggedIn,
