d5691430f3f87a0c63223af6da73475900b7f2aa
Monica Ong <myo3@cornell.edu>
2017-06-06 17:19:28 -0700

    PR changes

--- Objective-C Demo App/iOS-SDK-Demo/ALEventTrackingViewController.m
+++ Objective-C Demo App/iOS-SDK-Demo/ALEventTrackingViewController.m
@@ -68,16 +68,18 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: @"rootPrototype"];
-    if(!cell){
-        cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleSubtitle reuseIdentifier: @"rootPrototype"];
+    static NSString *const kCellIdentifier = @"rootPrototype";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: kCellIdentifier];
+    if ( !cell )
+    {
+        cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleSubtitle reuseIdentifier: kCellIdentifier];
     }
     
     cell.textLabel.text = self.events[indexPath.row].name;
     cell.detailTextLabel.text = self.events[indexPath.row].purpose;
     
     return cell;
-    
 }
 
 #pragma mark - Table View Delegate
@@ -88,164 +90,111 @@
     
     ALEventService *eventService = [ALSdk shared].eventService;
     
-    switch (indexPath.row)
+    ALDemoEvent *event = self.events[indexPath.row];
+    [self setTitle: event.name];
+    
+    switch ( indexPath.row )
     {
-        case 0: {
-            [self setTitle: @"User began checkout"];
+        case 0:
+        {
             [eventService trackEvent: kALEventTypeUserBeganCheckOut
-                          parameters: @{
-                                        kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID",
+                          parameters: @{kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID",
                                         kALEventParameterRevenueAmountKey     : @"PRICE OF ITEM",
-                                        kALEventParameterRevenueCurrencyKey   : @"3-LETTER CURRENCY CODE"
-                                        }
-             ];
+                                        kALEventParameterRevenueCurrencyKey   : @"3-LETTER CURRENCY CODE"}];
             break;
         }
         case 1: {
-            [self setTitle: @"User added item to cart"];
+            
             [eventService trackEvent: kALEventTypeUserAddedItemToCart
-                          parameters: @{
-                                        kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID"
-                                        }
-             ];
+                          parameters: @{kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID"}];
             break;
         }
-        case 2: {
-            [self setTitle: @"User completed achievement"];
+        case 2:
+        {
             [eventService trackEvent: kALEventTypeUserCompletedAchievement
-                          parameters: @{
-                                        kALEventParameterCompletedAchievementKey : @"ACHIEVEMENT NAME OR ID"
-                                        }
-             ];
+                          parameters: @{kALEventParameterCompletedAchievementKey : @"ACHIEVEMENT NAME OR ID"}];
             break;
         }
         case 3: {
-            [self setTitle: @"User completed checkout"];
             [eventService trackEvent: kALEventTypeUserCompletedCheckOut
-                          parameters: @{
-                                        kALEventParameterCheckoutTransactionIdentifierKey : @"UNIQUE TRANSACTION ID",
-                                        kALEventParameterProductIdentifierKey     : @"PRODUCT SKU OR ID",
-                                        kALEventParameterRevenueAmountKey         : @"AMOUNT OF MONEY SPENT",
-                                        kALEventParameterRevenueCurrencyKey       : @"3-LETTER CURRENCY CODE"
-                                        }
-             ];
+                          parameters: @{kALEventParameterCheckoutTransactionIdentifierKey : @"UNIQUE TRANSACTION ID",
+                                        kALEventParameterProductIdentifierKey             : @"PRODUCT SKU OR ID",
+                                        kALEventParameterRevenueAmountKey                 : @"AMOUNT OF MONEY SPENT",
+                                        kALEventParameterRevenueCurrencyKey               : @"3-LETTER CURRENCY CODE"}];
             break;
         }
         case 4: {
-            [self setTitle: @"User completed level"];
             [eventService trackEvent: kALEventTypeUserCompletedLevel
-                          parameters: @{
-                                        kALEventParameterCompletedLevelKey : @"LEVEL NAME OR NUMBER"
-                                        }
-             ];
+                          parameters: @{kALEventParameterCompletedLevelKey : @"LEVEL NAME OR NUMBER"}];
             break;
         }
         case 5: {
-            [self setTitle: @"User created reservation"];
             [eventService trackEvent: kALEventTypeUserCreatedReservation
-                          parameters: @{
-                                        kALEventParameterProductIdentifierKey     : @"PRODUCT SKU OR ID",
-                                        kALEventParameterReservationStartDateKey  : @"START NSDATE",
-                                        kALEventParameterReservationEndDateKey    : @"END NSDATE"
-                                        }
-             ];
+                          parameters: @{kALEventParameterProductIdentifierKey    : @"PRODUCT SKU OR ID",
+                                        kALEventParameterReservationStartDateKey : @"START NSDATE",
+                                        kALEventParameterReservationEndDateKey   : @"END NSDATE"}];
             break;
         }
         case 6: {
-            [self setTitle: @"Tracking in app purchase"];
             SKPaymentTransaction *transaction = [[SKPaymentTransaction alloc] init]; // from paymentQueue:updatedTransactions:
             //SKProduct* product = ...; // Appropriate product (matching productIdentifier property to SKPaymentTransaction);
             [eventService trackInAppPurchaseWithTransactionIdentifier: transaction.transactionIdentifier
-                                                           parameters: @{
-                                                                         kALEventParameterRevenueAmountKey : @"AMOUNT OF MONEY SPENT",
+                                                           parameters: @{kALEventParameterRevenueAmountKey : @"AMOUNT OF MONEY SPENT",
                                                                          kALEventParameterRevenueCurrencyKey : @"3-LETTER CURRENCY CODE",
-                                                                         kALEventParameterProductIdentifierKey : @"product.productIdentifier" //product.productIdentifier
-                                                                         }
-             ];
+                                                                         kALEventParameterProductIdentifierKey : @"product.productIdentifier"}]; //product.productIdentifier
             break;
         }
         case 7: {
-            [self setTitle: @"User logged in"];
             [eventService trackEvent: kALEventTypeUserLoggedIn
-                          parameters: @{
-                                        kALEventParameterUserAccountIdentifierKey : @"USERNAME"
-                                        }
-             ];
+                          parameters: @{kALEventParameterUserAccountIdentifierKey : @"USERNAME"}];
             break;
         }
         case 8: {
-            [self setTitle: @"User provided payment information"];
             [eventService trackEvent: kALEventTypeUserProvidedPaymentInformation];
             break;
         }
         case 9: {
-            [self setTitle: @"User created account"];
             [eventService trackEvent: kALEventTypeUserCreatedAccount
-                          parameters: @{
-                                        kALEventParameterUserAccountIdentifierKey : @"USERNAME"
-                                        }
-             ];
+                          parameters: @{kALEventParameterUserAccountIdentifierKey : @"USERNAME"}];
             break;
         }
         case 10: {
-            [self setTitle: @"User executed search"];
             [eventService trackEvent: kALEventTypeUserExecutedSearch
-                          parameters: @{
-                                        kALEventParameterSearchQueryKey : @"USER'S SEARCH STRING"
-                                        }
-             ];
+                          parameters: @{kALEventParameterSearchQueryKey : @"USER'S SEARCH STRING"}];
             break;
         }
         case 11: {
-            [self setTitle: @"User sent invitation"];
             [eventService trackEvent: kALEventTypeUserSentInvitation];
             break;
         }
         case 12: {
-            [self setTitle: @"User shared link"];
             [eventService trackEvent: kALEventTypeUserSharedLink];
             break;
         }
         case 13: {
-            [self setTitle: @"User spent virtual currency"];
             [eventService trackEvent: kALEventTypeUserSpentVirtualCurrency
-                          parameters: @{
-                                        kALEventParameterVirtualCurrencyAmountKey : @"NUMBER OF COINS SPENT",
-                                        kALEventParameterVirtualCurrencyNameKey : @"CURRENCY NAME"
-                                        }
-             ];
+                          parameters: @{kALEventParameterVirtualCurrencyAmountKey : @"NUMBER OF COINS SPENT",
+                                        kALEventParameterVirtualCurrencyNameKey : @"CURRENCY NAME"}];
+            
             break;
         }
         case 14: {
-            [self setTitle: @"User completed tutorial"];
             [eventService trackEvent: kALEventTypeUserCompletedTutorial];
             break;
         }
         case 15: {
-            [self setTitle: @"User viewed content"];
             [eventService trackEvent: kALEventTypeUserViewedContent
-                          parameters: @{
-                                        kALEventParameterContentIdentifierKey : @"SOME ID DESCRIBING CONTENT"
-                                        }
-             ];
+                          parameters: @{kALEventParameterContentIdentifierKey : @"SOME ID DESCRIBING CONTENT"}];
             break;
         }
         case 16: {
-            [self setTitle: @"User viewed product"];
             [eventService trackEvent: kALEventTypeUserViewedProduct
-                          parameters: @{
-                                        kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID"
-                                        }
-             ];
+                          parameters: @{kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID"}];
             break;
         }
         case 17: {
-            [self setTitle: @"User added item to wishlist"];
             [eventService trackEvent: kALEventTypeUserAddedItemToWishlist
-                          parameters: @{
-                                        kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID"
-                                        }
-             ];
+                          parameters: @{kALEventParameterProductIdentifierKey : @"PRODUCT SKU OR ID"}];
             break;
         }
         default: {
--- Swift Demo App/iOS-SDK-Demo/ALEventTrackingViewController.swift
+++ Swift Demo App/iOS-SDK-Demo/ALEventTrackingViewController.swift
@@ -55,7 +55,7 @@ class ALEventTrackingViewController: ALDemoBaseTableViewController
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "rootPrototype", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rootPrototype", for: indexPath)
         
         cell.textLabel?.text = events[indexPath.row].name
         cell.detailTextLabel?.text = events[indexPath.row].purpose
@@ -69,47 +69,44 @@ class ALEventTrackingViewController: ALDemoBaseTableViewController
     {
         super.tableView(tableView, didSelectRowAt: indexPath)
         
-        guard let eventService: ALEventService = ALSdk.shared()?.eventService else {
+        guard let eventService: ALEventService = ALSdk.shared()?.eventService else
+        {
             title = "Unable to track events because eventService is nil"
             return
         }
-
-        switch indexPath.row {
+        
+        title = events[indexPath.row].name
+        
+        switch indexPath.row
+        {
         case 0:
-            title = "User began checkout"
             eventService.trackEvent(kALEventTypeUserBeganCheckOut,
                                     parameters: [kALEventParameterProductIdentifierKey : "PRODUCT SKU OR ID",
                                                  kALEventParameterRevenueAmountKey     : "PRICE OF ITEM",
                                                  kALEventParameterRevenueCurrencyKey   : "3-LETTER CURRENCY CODE"])
         case 1:
-            title = "User added item to cart"
             eventService.trackEvent(kALEventTypeUserAddedItemToCart,
                                     parameters: [kALEventParameterProductIdentifierKey : "PRODUCT SKU OR ID"])
             
         case 2:
-            title = "User completed achievement"
             eventService.trackEvent(kALEventTypeUserCompletedAchievement,
                                     parameters: [kALEventParameterCompletedAchievementKey : "ACHIEVEMENT NAME OR ID"])
             
         case 3:
-            title = "User completed checkout"
             eventService.trackEvent(kALEventTypeUserCompletedCheckOut,
                                     parameters: [kALEventParameterCheckoutTransactionIdentifierKey : "UNIQUE TRANSACTION ID",
                                                  kALEventParameterProductIdentifierKey             : "PRODUCT SKU OR ID",
                                                  kALEventParameterRevenueAmountKey                 : "AMOUNT OF MONEY SPENT",
                                                  kALEventParameterRevenueCurrencyKey               : "3-LETTER CURRENCY CODE"])
         case 4:
-            title = "User completed level"
             eventService.trackEvent(kALEventTypeUserCompletedLevel,
                                     parameters: [kALEventParameterCompletedLevelKey : "LEVEL NAME OR NUMBER"])
         case 5:
-            title = "User created reservation"
             eventService.trackEvent(kALEventTypeUserCreatedReservation,
                                     parameters: [kALEventParameterProductIdentifierKey    : "PRODUCT SKU OR ID",
                                                  kALEventParameterReservationStartDateKey : "START NSDATE",
                                                  kALEventParameterReservationEndDateKey   : "END NSDATE"])
         case 6:
-            title = "Tracking in app purchase"
             //In-App Purchases
             // let transaction: SKPaymentTransaction = ... // from paymentQueue:updatedTransactions:
             //let product: SKProduct = ... // Appropriate product (matching productIdentifier property to SKPaymentTransaction)
@@ -118,46 +115,35 @@ class ALEventTrackingViewController: ALDemoBaseTableViewController
                                                          kALEventParameterRevenueCurrencyKey   : "3-LETTER CURRENCY CODE",
                                                          kALEventParameterProductIdentifierKey : "product.productIdentifier"]) //product.productIdentifier
         case 7:
-            title = "User logged in"
             eventService.trackEvent(kALEventTypeUserLoggedIn,
                                     parameters: [kALEventParameterUserAccountIdentifierKey : "USERNAME"])
         case 8:
-            title = "User provided payment information"
             eventService.trackEvent(kALEventTypeUserProvidedPaymentInformation)
             
         case 9:
-            title = "User created account"
             eventService.trackEvent(kALEventTypeUserCreatedAccount,
                                     parameters: [kALEventParameterUserAccountIdentifierKey : "USERNAME"])
             
         case 10:
-            title = "User executed search"
             eventService.trackEvent(kALEventTypeUserExecutedSearch,
                                     parameters: [kALEventParameterSearchQueryKey : "USER'S SEARCH STRING"])
         case 11:
-            title = "User sent invitation"
             eventService.trackEvent(kALEventTypeUserSentInvitation)
         case 12:
-            title = "User shared link"
             eventService.trackEvent(kALEventTypeUserSharedLink)
         case 13:
-            title = "User spent virtual currency"
             eventService.trackEvent(kALEventTypeUserSpentVirtualCurrency,
                                     parameters: [kALEventParameterVirtualCurrencyAmountKey : "NUMBER OF COINS SPENT",
                                                  kALEventParameterVirtualCurrencyNameKey   : "CURRENCY NAME"])
         case 14:
-            title = "User completed tutorial"
             eventService.trackEvent(kALEventTypeUserCompletedTutorial)
         case 15:
-            title = "User viewed content"
             eventService.trackEvent(kALEventTypeUserViewedContent,
                                     parameters: [kALEventParameterContentIdentifierKey : "SOME ID DESCRIBING CONTENT"])
         case 16:
-            title = "User viewed product"
             eventService.trackEvent(kALEventTypeUserViewedProduct,
                                     parameters: [kALEventParameterProductIdentifierKey : "PRODUCT SKU OR ID"])
         case 17:
-            title = "User added item to wishlist"
             eventService.trackEvent(kALEventTypeUserAddedItemToWishlist,
                                     parameters: [kALEventParameterProductIdentifierKey : "PRODUCT SKU OR ID"])
         default:
