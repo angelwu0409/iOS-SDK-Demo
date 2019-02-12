aecdbdcc29910cf4c421969efb4731f88daf4d3d
Thomas So <thomas.m.so33@gmail.com>
2019-02-12 13:20:10 -0800

    Show how to set userIdentifier

--- Objective-C Demo App/iOS-SDK-Demo/ALDemoAppDelegate.m
+++ Objective-C Demo App/iOS-SDK-Demo/ALDemoAppDelegate.m
@@ -13,11 +13,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [ALSdk initializeSdkWithCompletionHandler:^(ALSdkConfiguration *configuration) {
+    [[ALSdk shared] initializeSdkWithCompletionHandler:^(ALSdkConfiguration *configuration) {
         // SDK finished initialization
     }];
     
+    // Mark that we are in a testing environment
     [ALSdk shared].settings.isTestAdsEnabled = YES;
+    
+    // Set an identifier for the current user. This identifier will be tied to various analytics events and rewarded video validation
+    [ALSdk shared].userIdentifier = @"support@applovin.com";
     
     [[UINavigationBar appearance] setTranslucent: NO];
     [[UINavigationBar appearance] setBarTintColor: [UIColor colorWithRed: 10.0f/255.0f green: 131.0f/255.f blue: 170.0f/255.0f alpha: 1.0f]];
--- Swift Demo App/iOS-SDK-Demo/AppDelegate.swift
+++ Swift Demo App/iOS-SDK-Demo/AppDelegate.swift
@@ -10,23 +10,26 @@ import UIKit
 import AppLovinSDK
 
 @UIApplicationMain
-class ALDemoAppDelegate: UIResponder, UIApplicationDelegate {
-
+class ALDemoAppDelegate: UIResponder, UIApplicationDelegate
+{
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
-        
-        ALSdk.initializeSdk { (configuration: ALSdkConfiguration) in
+        ALSdk.shared()!.initializeSdk { (configuration: ALSdkConfiguration) in
             // SDK finished initialization
         }
         
+        // Mark that we are in a testing environment
         ALSdk.shared()!.settings.isTestAdsEnabled = true
-
+        
+        // Set an identifier for the current user. This identifier will be tied to various analytics events and rewarded video validation
+        ALSdk.shared()!.userIdentifier = "support@applovin.com"
+        
         UINavigationBar.appearance().isTranslucent = false
         UINavigationBar.appearance().barTintColor = UIColor(red: 10.0/255.0, green: 131.0/255.0, blue: 170.0/255.0, alpha: 1.0)
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
-    
+        
         UINavigationBar.appearance().tintColor = UIColor.white
         
         return true
